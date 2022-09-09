6a169d3de45de5328affcece9c9474c57196c2c3
Julian Millar <julianjmillar@gmail.com>
2022-09-09 09:24:54 -0300

    Reglas sobre nombres de variables

---
+++ var.js
@@ -0,0 +1,26 @@
+console.clear();
+
+var firstNum = 10;
+console.log(firstNum);
+
+var secondNum = 15;
+console.log();
+
+// Camel case (recomendada)
+var firstNum;
+// Pascal case
+var FirstNum;
+// Snake case
+var first_num;
+
+// Reglas para nombrar variables
+// Nombres pueden contener letras, digitos, _ y $
+var mySubject1, math$;
+// Nombres no pueden empezar con un digito
+var 1numero;
+// Nombres pueden empezar con _ y $
+var _subjectmarks, $rankInClass;
+// Los nombres son sensibles a las mayusculas
+var x, X;
+// Los nombres no pueden ser palabras reservadas
+var var;
